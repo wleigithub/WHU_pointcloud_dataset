b5125fa4dd324a1140b78acde814d506711dc566
WangLei <wlei@whu.edu.cn>
2017-12-28 20:22:50 +0800

    Add files via upload

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+
+# Download original WHU TLS dataset (around 240M)
+wget --no-check-certificate 'https://drive.google.com/uc?authuser=0&id=10H5bhuIqdFL6iChpF0BbYjTGYqQVdXS1&export=download' -O WHU_VLS_PointCloud.zip
+unzip WHU_VLS_PointCloud.zip
+rm WHU_VLS_PointCloud.zip
